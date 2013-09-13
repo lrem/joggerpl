432f6fa84de8dee3a69637616c3b6e4c0ac2b980
Remigiusz Modrzejewski <lrem@maxnet.org.pl>
2013-09-13 18:37:55 +0200

    For a good start: logging in to the control panel with mechanize.py.

---
+++ joggerpl/session.py
@@ -0,0 +1,26 @@
+"""
+An abstraction over the HTML interface of the Jogger.pl control panel.
+"""
+
+
+import mechanize
+
+
+class Session(object):
+    """
+    Represents a session of the control panel.
+    """
+
+    def __init__(self):
+        self.browser = mechanize.Browser()
+
+    def login(self, user, password):
+        """
+        Attempt to log in using the given credentials.
+        """
+        br = self.browser
+        br.open('http://jogger.pl')
+        br.select_form(nr=0)
+        br.form['login_jabberid'] = user
+        br.form['login_jabberpass'] = password
+        br.submit()
